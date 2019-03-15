28627f729fa6298495450cb8259e145f9aa5cee0
Hellen S <hellensoloviy@gmail.com>
2019-03-15 10:51:16 +0200

    fix constant;

--- CountriesListDemo/Common/Extensions/String+Sizing.swift
+++ CountriesListDemo/Common/Extensions/String+Sizing.swift
@@ -12,7 +12,7 @@ import UIKit
 extension String {
     func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedStringKey.font: font], context: nil)
+        let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedString.Key.font: font], context: nil)
         return boundingBox.height
     }
 }
